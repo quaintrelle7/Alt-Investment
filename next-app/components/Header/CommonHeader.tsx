1bcc0fb265e5eaeae3c78ec714120d1bc94d9718
sharayu <sharayu@cooeyhealth.com>
2024-03-21 23:53:25 +0530

    removed fixed header

--- next-app/components/Header/CommonHeader.tsx
+++ next-app/components/Header/CommonHeader.tsx
@@ -4,7 +4,7 @@ import {Button, Flex, Heading, Image, Link} from '@chakra-ui/react'
 function CommonHeader() {
   return (
     <>
-    <Flex position={"sticky"} top={"0"} width={"100vw"} height={"3rem"} bg={"brand.primary"} px={10} py={10}>
+    <Flex width={"100vw"} height={"3rem"} bg={"brand.primary"} px={10} py={10}>
       <Flex width={"50%"} height={"100%"} align={"center"} justify={"flex-start"}>
         <Link marginRight={10} color={"white"} href='/'> Home </Link>
         <p color={"white"}>Wallet Address</p>
