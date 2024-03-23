0a08901e61da7ebac112a7088243f777e2948eca
sharayu <sharayu@cooeyhealth.com>
2024-03-24 01:58:01 +0530

    myportfolio

--- next-app/components/Homepage/AboutCompanies.tsx
+++ next-app/components/Homepage/AboutCompanies.tsx
@@ -1,20 +1,42 @@
 import React from 'react'
-import { Center, Flex, Heading, Stack, Image, Divider} from '@chakra-ui/react'
+import { Center, Flex, Heading, Stack, Image, Divider, Box} from '@chakra-ui/react'
 import {InfiniteMovingCards} from '../ui/infinite-moving-cards';
 
 type Props = {}
 
 
-
+ const companies = ["Polygon", "Filecoin", "Spheron", "Google", "AWS", "Hasura", "DigitalOcean", "Replit"
+    ]
 
 function AboutCompanies({}: Props) {
   return (
     <>
-    <Center px={100}>
-    <Stack height={"50vh"} my={20} >
-        <Heading>Invest in your favourite companies globally</Heading>
-    </Stack>
+    <Stack mx={20} pt={20} my={20}>
+
+    
+    <Flex  justify={"center"}>
+            <Center width={{base:"60%", md:"40%"}}fontSize={{base:"2xl", md:"50px"}} textAlign={"center"}>
+                <h1 color="brand.primary">Invest in your favourite companies globally</h1>
+            </Center>
+            
+        </Flex>
+    <Center px={100} py={50}>
+        
     </Center>
+    <Stack height={"50vh"}>
+    <Flex justifyContent={"space-between"} className='buildSlide'>
+                    {
+                        companies.map((item) => (
+                            <Box bg="brand.ternary" borderRadius={"20em"}  p="4" my={5} mx={5} key={item}>
+                                <Heading fontSize={20} fontWeight={"500"} color="brand.quinary">{item}</Heading>
+                            </Box>
+                        ))
+                    }
+    </Flex>
+    </Stack>
+    </Stack>
+     
+    
     </>
   )
 }
