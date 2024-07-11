eb5d24a6d63a0adbb2ac265412ff4a53f4583778
Sharayu <sharayushendre7@gmail.com>
2024-07-11 16:24:25 +0530

    major changes

--- next-app/pages/client.tsx
+++ next-app/pages/client.tsx
@@ -5,47 +5,55 @@ import Header from '@/components/Header/Header'
 import ClientSummary from '@/components/Client/ClientSummary'
 import ActiveInvoices from '@/components/Client/Invoices'
 import Invoices from '@/components/Client/Invoices'
-import AboutCompany from '@/components/Client/AboutCompany'
-import ClientDashboard from '@/components/Client/ClientDashboard'
-
+import AboutCompany from "@/components/Client/AboutCompany"
 
 type Props = {}
 
 function client({}: Props) {
-  return (
-    <>
-        <Header/>        
-        <ClientDashboard/>
-        
-   <Stack style={{marginTop:"150px", marginRight:"2.5rem"}}>
-    <Tabs variant='soft-rounded' ml={10}>
-      <TabList  >
-        <Tab bg={"brand.ternary"}  _selected={{bg:"brand.quinary", color:"brand.primary"}}>Summary</Tab>
-        <Tab bg={"brand.ternary"} mx="5"  _selected={{bg:"brand.quinary", color:"brand.primary"}}>Active Invoices</Tab>
+	return (
+		<>
+			<Header />
 
-        {/* Optional */}
-        <Tab bg={"brand.ternary"}  _selected={{bg:"brand.quinary", color:"brand.primary"}}>About Company</Tab>
+			<Stack style={{ marginTop: "150px", marginRight: "2.5rem" }}>
+				<Tabs variant="soft-rounded" ml={10}>
+					<TabList>
+						<Tab
+							bg={"brand.ternary"}
+							_selected={{ bg: "brand.quinary", color: "brand.primary" }}>
+							Summary
+						</Tab>
+						<Tab
+							bg={"brand.ternary"}
+							mx="5"
+							_selected={{ bg: "brand.quinary", color: "brand.primary" }}>
+							Active Invoices
+						</Tab>
 
-      </TabList>
-      <TabPanels bg="brand.ternary" mt="10">
-        <TabPanel>
-            <ClientSummary/>
-        </TabPanel>
-        <TabPanel>
-            <Invoices/>
-        </TabPanel>
-        <TabPanel>
-            <AboutCompany/>
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
-   </Stack>
-    <div style={{ bottom:"0", width:"auto"}}>
-        <Footer/>
-    </div>
-    
-    </>
-  )
+						{/* Optional */}
+						<Tab
+							bg={"brand.ternary"}
+							_selected={{ bg: "brand.quinary", color: "brand.primary" }}>
+							About Company
+						</Tab>
+					</TabList>
+					<TabPanels bg="brand.ternary" mt="10">
+						<TabPanel>
+							<ClientSummary />
+						</TabPanel>
+						<TabPanel>
+							<Invoices />
+						</TabPanel>
+						<TabPanel>
+							<AboutCompany />
+						</TabPanel>
+					</TabPanels>
+				</Tabs>
+			</Stack>
+			<div style={{ bottom: "0", width: "auto" }}>
+				<Footer />
+			</div>
+		</>
+	)
 }
 
 export default client
