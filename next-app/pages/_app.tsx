bd7a8c9269da56c4258833b53f5e1e1d94f9a54f
sharayu <sharayu@cooeyhealth.com>
2024-07-16 16:34:39 +0530

    commenting sign in for now

--- next-app/pages/_app.tsx
+++ next-app/pages/_app.tsx
@@ -57,7 +57,7 @@ export default function App({ Component, pageProps }: AppProps) {
 		<WagmiProvider config={config}>
 			<SessionProvider refetchInterval={0} session={pageProps.session}>
 				<QueryClientProvider client={queryClient}>
-					<RainbowKitSiweNextAuthProvider getSiweMessageOptions={getSiweMessageOptions}> 
+					{/* <RainbowKitSiweNextAuthProvider getSiweMessageOptions={getSiweMessageOptions}>  */}
 					<RainbowKitProvider
 						coolMode
 						theme={darkTheme({
@@ -73,7 +73,7 @@ export default function App({ Component, pageProps }: AppProps) {
 							</ChakraProvider>
 						</Layout>
 					</RainbowKitProvider>
-					</RainbowKitSiweNextAuthProvider>
+					{/* </RainbowKitSiweNextAuthProvider> */}
 				</QueryClientProvider>
 			</SessionProvider>
 		</WagmiProvider>
